e82b5466c84e911cac4e75e5b3c468f6c18a7bdf
Tharushipa <158550141+Tharushipa@users.noreply.github.com>
2024-07-09 22:08:57 +0530

    changed placeholder  name

--- src/components/AppointmentForm.jsx
+++ src/components/AppointmentForm.jsx
@@ -142,7 +142,7 @@ const AppointmentForm = () => {
             </select>
             <input
               type="date"
-              placeholder="Appointment Date"
+              placeholder="Appointment Date  kk"
               value={appointmentDate}
               onChange={(e) => setAppointmentDate(e.target.value)}
             />
